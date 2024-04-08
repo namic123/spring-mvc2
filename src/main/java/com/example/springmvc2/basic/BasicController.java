df8204c00489035909e1a0ccc5e7fc2db8244823
jay sung <namic123@naver.com>
2024-04-08 16:15:51 +0900

    타임리프 기본 사용법. model attribute 출력하기. escape와 unescape 사용

---
+++ src/main/java/com/example/springmvc2/basic/BasicController.java
@@ -0,0 +1,21 @@
+package com.example.springmvc2.basic;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/basic")
+public class BasicController {
+@GetMapping("text-basic")
+    public String textBasic(Model model){
+    model.addAttribute("data", "Hello Spring!");
+    return "basic/text-basic";
+}
+    @GetMapping("text-unescaped")
+    public String textUnescaped(Model model){
+        model.addAttribute("data", "<b>Hello Spring!</b>");
+        return "basic/text-unescaped";
+    }
+}
